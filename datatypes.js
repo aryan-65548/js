f1ac035dc9b492e1f90b2b0298f81d81868c7b3a
Aryan Patel <aryan.patel25jan@gmail.com>
2026-02-13 11:10:37 +0530

    Add files via upload
    basic practice

---
+++ datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) we r using node JS so alert is not defined
+console.log(3+3);
+ // we can use console.log in node JS to print something on the console
+let name = "aryan"
+let age = 18
+let isLoggedIn = false
+//symbol is a unique data type in js
+
---
+++ variable.js
@@ -0,0 +1,12 @@
+const account_Id = "43556";
+let accountEmail = "aryan@gmail.com"
+var accountpassword = "goat"
+accountCity = "Jaipur"
+
+//account_Id = 2
+accountEmail = "ap.com"
+accountpassword = "goat123"
+accountCity = "Mumbai"
+
+
+console.table([account_Id, accountEmail, accountpassword, accountCity]);
